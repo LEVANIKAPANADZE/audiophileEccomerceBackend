553bdfcbc71aded2b6e1920cdd68bc84bb8d753a
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-01-04 00:25:41 +0400

    Added api key

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+API_KEY = "X9f#2aB7!dE4$kL1"
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,33 +16,39 @@ class User(db.Model):
     password = db.Column(db.String(50))
     basket = db.Column(db.JSON, default=list)  
 
+def require_api_key(func):
+    def wrapper(*args, **kwargs):
+        key = request.headers.get("x-api-key")
+        if key != API_KEY:
+            return jsonify({"error": "Forbidden"}), 403
+        return func(*args, **kwargs)
+    wrapper.__name__ = func.__name__
+    return wrapper
+
 
 @app.route("/users/", methods=["POST"])
+@require_api_key
 def register():
     data = request.json
-
     user = User(
         username=data["username"],
         email=data["email"],
         password=data["password"],
-        basket=[]  
+        basket=[]
     )
-
     db.session.add(user)
     db.session.commit()
-
     return jsonify({"id": user.id})
 
 
 @app.route("/login/", methods=["POST"])
+@require_api_key
 def login():
     data = request.json
-
     user = User.query.filter_by(
         email=data["email"],
         password=data["password"]
     ).first()
-
     if user:
         return jsonify({"true_or_false": True, "id": user.id})
     else:
@@ -49,6 +56,7 @@ def login():
 
 
 @app.route("/users/", methods=["GET"])
+@require_api_key
 def get_users():
     users = User.query.all()
     return jsonify([
@@ -57,12 +65,13 @@ def get_users():
             "username": u.username,
             "email": u.email,
             "password": u.password,
-            "basket": u.basket 
+            "basket": u.basket
         } for u in users
     ])
 
 
 @app.route("/users/<int:id>", methods=["GET"])
+@require_api_key
 def get_user(id):
     u = User.query.get(id)
     if u:
@@ -71,7 +80,7 @@ def get_user(id):
             "username": u.username,
             "email": u.email,
             "password": u.password,
-            "basket": u.basket 
+            "basket": u.basket
         })
     return jsonify({"error": "User not found"}), 404
 
